e0715f46fa1ee31355bf5c01dec6065c593eddf4
Amrit Rai <mail.amritrai@gmail.com>
2021-04-10 16:49:38 +0530

    Added new Class gitClass.java

---
+++ Framework/src/test/java/Cucumber/Framework/gitClass.java
@@ -0,0 +1,16 @@
+package Cucumber.Framework;
+
+public class gitClass {
+	public static void howzit() { 
+		System.out.println("How are you?");
+	
+	
+
+}
+
+public static void main(String[] args) {
+	
+	howzit();
+	
+}
+}
